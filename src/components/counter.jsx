8071988ebdf3ad68f4e08138bce087dd8a1db5d2
Eileen Ninan <eileenninan@Eileens-MacBook-Pro.local>
2021-09-03 16:33:11 +0530

    Second Commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
         return (
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={this.handleIncrement(yarn)} className="btn btn-secondary btn-sm">Increment</button>
         </div>
      );
     }
